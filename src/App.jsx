861b59995a8a090ae2f4198e1d263b0673f4de4f
THANAYAMWATTA D.M.A.S.B <IT23839656@my.sliit.lk>
2026-01-07 22:37:16 +0530

    Fix: Switch to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Luxury from './pages/Luxury';
 import Neo from './pages/Neo';
@@ -6,7 +6,7 @@ import Organic from './pages/Organic';
 
 function App() {
   return (
-    <Router basename="/Real-state-fronted-demo">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/luxury" element={<Luxury />} />
